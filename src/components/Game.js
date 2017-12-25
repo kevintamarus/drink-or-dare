ee8225986605ffae6771038caabed4077a24df55
Kevin Tamarus Tang <kevintamarus@gmail.com>
2017-12-24 17:22:57 -0800

    json files added, buttons added

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,10 +1,25 @@
 import React, { Component } from 'react';
+import {Button, ButtonGroup} from 'react-bootstrap';
 
 export default class Game extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: [],
+      index: null
+    }
+  }
+
+  componentDidMount() {
+  }
+
   render() {
     return (
       <div style={styles.container}>
-        this is the Game component
+        <ButtonGroup>
+          <Button bsClass="btn btn-w-m btn-lg btn-success">Truth</Button>
+          <Button bsClass="btn btn-w-m btn-lg btn-warning">Dare</Button>
+        </ButtonGroup>
       </div>
     )
   }
